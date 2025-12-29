e19a05b03a54a81018322efc0879dd196c2358d2
Ozan Böce <ozan.bocee@gmail.com>
2025-12-30 00:06:58 +0300

    Add Azure Key Vault and PostgreSQL integration
    Introduced @azure/identity, @azure/keyvault-secrets, and pg dependencies. Added a new /hello endpoint that retrieves the database password from Azure Key Vault and connects to a PostgreSQL database, demonstrating secure secret management and database connectivity.

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express');
+const { Client } = require('pg');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { SecretClient } = require('@azure/keyvault-secrets');
+
 const app = express();
 const port = process.env.PORT || 2222;
 
+// Azure Key Vault Bilgileri
+const kvUrl = "https://keyv-project-kemalozan.vault.azure.net/";
+const credential = new DefaultAzureCredential();
+const secretClient = new SecretClient(kvUrl, credential);
 
+// Health check endpoint for Azure Web App
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
 
+
+app.get('/hello', async (req, res) => {
+    try {
+        // Şifreyi Key Vault'tan çekiyoruz 
+        const dbPasswordSecret = await secretClient.getSecret("db-password");
+        const dbPassword = dbPasswordSecret.value;
+
+        // PostgreSQL Bağlantısı
+        const client = new Client({
+            host: 'project-sql.postgres.database.azure.com',
+            user: 'kemalozan',
+            password: dbPassword,
+            database: 'postgres',
+            port: 5432,
+            ssl: true
+        });
+
+        await client.connect();
+        res.send("Merhaba Grup 12! Node.js ile Veritabanına ve Key Vault'a başarıyla bağlandık.");
+        await client.end();
+    } catch (err) {
+        res.status(500).send("Hata: " + err.message);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda çalışıyor.`);
